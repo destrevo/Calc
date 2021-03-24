f27b12655b30580f4136d062378c2bc5c67d28ea
destrevo <maczko.andor@gmail.com>
2021-03-24 15:26:45 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
+
+const numbers = [1,2,3,4,5,6,7,8,9,0]
+const operators =["+","-"]
 
 function App() {
+  const [array, setarray] = useState([])
+  const [dC, setDc] = useState()
+
+  const ClickedButton = (number) => {
+    if (array[array[0]] === "0") {
+      return
+    } 
+    if (array[array.length-1] === "0" && (array[array.length-2] ==="+" || array[array.length-2] === "-")) {
+      return
+    } 
+     setarray(array.concat( number.toString()))
+    };
+  const Operator = (xS) => {
+    if(array[array.length-1]==='+'|| array[array.length-1]==='-' ){
+      return
+    }
+    if (array.length === 0 && xS === '+'){
+      return
+    }
+   setarray(array.concat(xS))
+    }
+  
+ const ResultBtn = () => {
+  if(array[array.length-1]==='+' || array[array.length-1]==='-'){
+    DeleteBtn()
+    }
+ /* ebben a környezetben biztonságos az EVAL mivel kontrollált inputok vannak, így nincs szükség külső algoritmus használatára */
+    setDc(eval(array.join("")))
+
+ };
+
+ 
+ const DeleteBtn = () => {
+  array.splice(array.length-1, 1)
+  let res = Array.of(...array)
+  setarray(res)
+ };
+ const ClearBtn = () => {
+  setarray([])
+  setDc(null)
+ };
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="display">{array}<h2>{dC}</h2></div>
+      
+      <div className="controls">
+        {numbers.map((num,key) => {
+          return <button key={key} type="number"onClick={()=>{(ClickedButton(num))}}>{num}</button>
+        })}
+        <button onClick={()=>{(ClearBtn())}}>Clear</button>
+        <button onClick={()=>{(DeleteBtn())}}>Delete</button>
+        <div className="operators">
+        {operators.map((xS,key) => {
+          return <button key={key} type="text"onClick={()=>{(Operator(xS))}}>{xS}</button>
+        })}
+        </div>
+        <button onClick={()=>{(ResultBtn())}}>=</button>
+      </div>
     </div>
   );
 }
